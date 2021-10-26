13d7bee0bae84f0fd5c7b316326856e1f3e244ae
Carly Durand <carlydurand@Carlys-MBP-2.lan>
2021-10-26 11:04:45 -0400

    Created Todo App

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Hacktoberfest2021
 Hacktoberfest 2021 project
+
+# Run Yarn inside hacktoberfest2021 directory
+yarn
+
+yarn start
--- hacktoberfest2021/src/index.js
+++ hacktoberfest2021/src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            text: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>TODO</h3>
+                <TodoList items={this.state.items}/>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if(!this.state.text.length) {
+            return;
+        }
+        const newItem = {
+            id: Date.now(),
+            text: this.state.text,
+        }
+        this.setState(prev => ({
+            items: prev.items.concat(newItem),
+            text: ''
+        }));
+    }
+}
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Todo />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
