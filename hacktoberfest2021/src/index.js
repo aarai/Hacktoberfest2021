0d59c9e2562f6c71fa3a9deb0d8c368c9f50eda0
alenquer <alenquer@pm.me>
2021-10-28 21:08:14 -0300

    persist local data

--- hacktoberfest2021/src/index.js
+++ hacktoberfest2021/src/index.js
@@ -1,75 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Todos extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { todos: [], text: '' };
-        this.removeTodo = this.removeTodo.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { todos: [], text: "" };
+    this.removeTodo = this.removeTodo.bind(this);
+  }
 
-    addTodo(e) {
-        e.preventDefault();
-        this.setState({ 
-        	todos: [ this.state.text, ...this.state.todos ],
-        	text: ''
-        });
-    }
+  addTodo(e) {
+    e.preventDefault();
+    if (!this.state.text) return alert("Todo name is required!");
+    this.setState({
+      todos: [this.state.text, ...this.state.todos],
+      text: "",
+    });
+  }
 
-    removeTodo(name, i){
-        let todos = this.state.todos.slice();
-        todos.splice(i, 1);
-        this.setState({
-            todos
-        });
-    }
+  removeTodo(name, i) {
+    let todos = this.state.todos.slice();
+    todos.splice(i, 1);
+    this.setState({
+      todos,
+    });
+  }
 
-    updateValue(e) {
-        this.setState({ text: e.target.value})
-    }
+  updateValue(e) {
+    this.setState({ text: e.target.value });
+  }
 
-    render() {
-        return(
-            <div>
-                <h1>Todo List</h1>  
-                <TodoList todos={this.state.todos} removeTodo={this.removeTodo}/>
-                <form onSubmit = {(e) => this.addTodo(e)}>
-                    <input
-                        placeholder="Add Todo"
-                        value={this.state.text}
-                        onChange={(e) => {this.updateValue(e)}}
-                    />
-                    <button className="btn" type="submit">Add Todo</button>
-                </form>
-            </div>
-        );
+  componentDidMount() {
+    try {
+      const database = JSON.parse(localStorage.getItem("todos"));
+      this.setState({
+        todos: database,
+      });
+    } catch (e) {
+      console.log("Database error!");
     }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <TodoList todos={this.state.todos} removeTodo={this.removeTodo} />
+        <form onSubmit={(e) => this.addTodo(e)}>
+          <input
+            placeholder="Add Todo"
+            value={this.state.text}
+            onChange={(e) => {
+              this.updateValue(e);
+            }}
+          />
+          <button className="btn" type="submit">
+            Add Todo
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 class TodoList extends React.Component {
+  removeItem(item, i) {
+    this.props.removeTodo(item, i);
+  }
 
-    removeItem(item, i) {
-        this.props.removeTodo(item, i);
-    }
-
-    render() {
-        return(
-            <ul>
-                { this.props.todos.map((todo,i) => {
-                    return <li key={i}>{ todo } <button className="btn2" onClick={() => { this.removeItem(todo, i)}}>Delete</button></li>
-                })}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo, i) => {
+          return (
+            <li key={i}>
+              {todo}{" "}
+              <button
+                className="btn2"
+                onClick={() => {
+                  this.removeItem(todo, i);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
-//ReactDOM.render(<Todos/>, document.getElementById('app'))
-
-
-// ========================================
-
-ReactDOM.render(
-    <Todos />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Todos />, document.getElementById("root"));
